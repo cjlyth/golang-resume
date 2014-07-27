32d92d89cd291156643ded8205d1fdbf83bdcc0b
Christopher J Lyth <cjlyth@gmail.com>
2014-07-27 15:32:02 -0400

    updated a path in the script

--- go-resume-scripts/serve.sh
+++ go-resume-scripts/serve.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 
-pushd /home/cjlyth/go-work/src/github.com/cjlyth &> /dev/null
+pushd $HOME/go-work/src/github.com/cjlyth &> /dev/null
 
 goapp serve go-resume-web/dispatch.yaml go-resume-web/app.yaml go-resume-api/api.yaml
 
